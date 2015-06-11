646f95e7e2deb76d0733380e35ad91b6eaf20cc4
Ignacio Giagante <igiagante@gmail.com>
2015-06-11 17:57:22 -0300

    Refactoring Image Service Code

--- routers/image_router.js
+++ routers/image_router.js
@@ -5,17 +5,18 @@ var imageController = require('../controllers/image_controller')
 //get all images
 router.get('/:plant_id', imageController.getImages);
 
+//add image to plant
+router.post('/:plant_id', imageController.postImage);
+
+//add image to plant
+router.put('/:plant_id/:mainImage', imageController.addImage);
+
 //get all image files
 router.get('/files/:plant_id', imageController.getImageFiles);
 
 //get main image for one plant
 //router.get('/:plant_id', imageController.getMainImage);
 
-//add image to plant
-router.put('/:plant_id/:mainImage', imageController.addImage);
-
-//add image to plant
-router.post('/:plant_id', imageController.postImage);
 
 //delete a plant
 router.delete('/:plant_id', imageController.deleteImage);
--- services/image_service.js
+++ services/image_service.js
@@ -7,9 +7,110 @@ var Img = require('../models/image.js'),
 
 var upload = function(image, mainImage, folder, callback){
 
-	var fileName = image.originalname
+	var fileName = image.originalname;
 
-	var dirPath = process.cwd() + '/public' + '/images/uploads/' + folder;
+	//path to write files
+    var newPath = dirPath + '/fullsize/' + fileName;
+    var thumbPath = dirPath + '/thumbs/' + fileName;
+
+	var directories = [mainPath, fullsizeImagePath, thumbImagePath];
+
+	var createImageDirectory = function(folder, createImageDirectoryCallback){
+
+		var mainPath = process.cwd() + '/public' + '/images/uploads/' + folder;
+		var fullsizeImagePath = dirPath + '/fullsize/';
+		var thumbImagePath = dirPath + '/thumbs/';
+
+		var iterator = function(d, cb){
+		  fs.mkdir(d, cb);
+		}
+
+		async.each(directories, iterator, function(err){
+		  if (err) return createImageDirectoryCallback(err);
+		  logger.log('all dirs created, moving on');
+		  createImageDirectoryCallback();
+		});
+	};
+
+	var readImageFile = function(image, readImageFileCallback){
+
+		fs.readFile(image.path, function (err, data) {
+			fs.rename(image.path, newPath);
+			readImageFileCallback();
+		}
+	};
+
+	var writeImageFile = function(writeImageCallback){
+		
+		fs.writeFile(newPath, data, function (err) {
+
+			console.log('writing file in ... ' + newPath);
+
+			if(err) {
+				logger.debug('todo mal ------ ' + err)
+				return writeImageCallback(err);
+			}
+			writeImageCallback();
+		}
+	};
+
+	var resizeImage = function(resizeImageCallback){
+
+		im.resize({ srcPath: newPath, dstPath: thumbPath, width: 200 }, 
+
+			function(err, stdout, stderr){
+		
+			if (err) {
+				console.log('The thumbnail image couldnt be saved');
+				return resizeImageCallback(err);
+			}
+	  
+	  		console.log('resized image to fit within 200x200px');
+
+			return resizeImageCallback(undefined, imageDetail.id);	
+		});	
+	};
+
+	var createImage = function(createImageCallback){
+
+		//paths to urls
+		var urlPath = '/images/uploads/' + folder + '/fullsize/' + fileName;
+		var thumbnailUrlPath = '/images/uploads/' + folder + '/thumbs/'  + fileName;
+
+		Img.create({url: urlPath, thumbnailUrl: thumbnailUrlPath, main: mainImage, name: fileName}, function(err, imageDetail) {
+			if(err) {
+				console.log('The full image couldnt be saved');
+				return createImageCallback(err);
+			}
+
+			console.log(' image fullsize uploaded ' + image.originalname);
+
+			console.log(' resizing fullsize uploaded ' + image.originalname);		
+
+			createImageCallback();	
+		});
+	};
+
+
+	//Image Creation Process 
+	async.waterfall([
+
+		//Create directory to persist user plants' images
+	    async.apply(createImageDirectory(createImageDirectoryCallback)),
+
+		//Read image file
+	    async.apply(readImageFile(readImageFileCallback)),
+
+	    //Resize image in order to create a thumbnail
+	    async.apply(resizeImage(resizeImageCallback)),
+
+	    //Write image file in disk
+	    async.apply(writeImageFile(writeImageCallback)),
+
+	    //Save image data in database
+	    async.apply(createImage(createImageCallback))
+
+	});
 
 	fs.readFile(image.path, function (err, data) {
 		if(err) {
@@ -46,30 +147,7 @@ var upload = function(image, mainImage, folder, callback){
 			console.log('write file to uploads/fullsize folder')
 
 
-			Img.create({url: urlPath, thumbnailUrl: thumbnailUrlPath, main: mainImage, name: fileName}, function(err, imageDetail) {
-				if(err) {
-					console.log('The full image couldnt be saved');
-					return callback(err);
-				}
-
-				console.log(' image fullsize uploaded ' + image.originalname);
-
-				console.log(' resizing fullsize uploaded ' + image.originalname);
-
-				im.resize({ srcPath: newPath, dstPath: thumbPath, width: 200 }, 
-
-					function(err, stdout, stderr){
 				
-					if (err) {
-						console.log('The thumbnail image couldnt be saved');
-						return callback(err);
-					}
-			  
-			  		console.log('resized image to fit within 200x200px');
-
-					return callback(undefined, imageDetail.id);	
-				});			
-			});	
 		});		 
 	});
 };
