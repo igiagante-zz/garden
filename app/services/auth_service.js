2301061a4123bc653e59df7dfd9de38d0fc0c613
Ignacio <igiagante@gmail.com>
2016-08-26 15:36:40 -0300

    fix 2

--- app/services/auth_service.js
+++ app/services/auth_service.js
@@ -3,17 +3,12 @@
  */
 
 var jwt = require('jwt-simple'),
-    moment = require('moment'),
     auth = require('../../config/auth'),
     User = require('../models/user');
 
 var isUserAuthenticated = function (token, isUserAuthenticatedCallback) {
 
     var payload = jwt.decode(token, auth.secret);
-
-    if(payload.exp <= moment().unix()) {
-        return res.status(401).send({message: "The token has expired"});
-    }
 
     User.findOne({
         _id: payload.sub
